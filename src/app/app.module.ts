72bc225dd5902dd6607578b5dd8b227a4fb3a0f2
Goldpm <lowedavidadam@gmail.com>
2020-04-01 16:02:53 -0400

    merged routing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CompTwoComponent } from './comp-two/comp-two.component';
 
+const routes: Routes = [
+  { path: 'comp-two', component: CompTwoComponent}
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    CompTwoComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CompTwoComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
