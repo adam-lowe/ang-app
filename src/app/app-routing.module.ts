ebcf86983612c586d30b4e38bf3e9e6e3431b085
Goldpm <lowedavidadam@gmail.com>
2020-04-01 15:07:01 -0400

    created route to comp-two

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router'
+import { CompTwoComponent } from './comp-two/comp-two.component';
 
+
+const routes: Routes = [
+  { path: 'comp-two', component: CompTwoComponent }
+]
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
